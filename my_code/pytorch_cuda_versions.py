ea5032298d7defa4c07c27fb0896276638a19144
mohali <mohamed.ali@blnk.ai>
2024-04-24 12:46:37 +0200

    init

---
+++ my_code/pytorch_cuda_versions.py
@@ -0,0 +1,9 @@
+import torch
+print("What is my torch version?", torch.__version__)
+print("---------------")
+print("what is my torch cuda version?",torch.version.cuda)
+print("---------------")
+print("Is torch using GPU? ", torch.cuda.is_available())
+print("---------------")
+print("all contained and isolated in this container")
+print("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥")
